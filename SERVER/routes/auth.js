4fa9e7b0824eee1cef18d66a41cffc23604b0193
chesscrafted-dev <chesscrafted@gmail.com>
2026-03-19 11:35:00 +0530

    fix nodemailer issue

--- SERVER/routes/auth.js
+++ SERVER/routes/auth.js
@@ -6,11 +6,18 @@ const User = require('../models/User');
 const crypto = require('crypto');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // Use STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  tls: {
+    rejectUnauthorized: false // Helps with certain cloud network restrictions
+  },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
 });
 
 router.post('/request-otp', async (req, res) => {
